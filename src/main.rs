b74996f2749d6172cedb6a81ecb03aa76b07666a
deciduously <ben@deciduously.com>
2019-04-18 08:26:37 -0400

    Working, pre-clippy

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate itertools;
 
+use rand::{seq::SliceRandom, thread_rng, Rng};
 use regex::Regex;
 use std::{
     collections::HashMap,
@@ -29,19 +30,37 @@ fn main() {
     let file_str = read_file().unwrap();
     let words = split_words(&file_str);
 
-    let transition: HashMap<(String, String), Vec<String>> = HashMap::new();
+    let mut transition: HashMap<(String, String), Vec<String>> = HashMap::new();
 
-    // loop through three words at a time:
-    // for w0, w1, w2 in zip(words[0:], words[1:], words[2:]):
-    //     transition[w0, w1].append(w2)
-
-    for (w1, w2, w3) in izip!(&words, &words[1..], &words[2..]) {
-        // if doesnt exist, create
-        // if does exist, append
-
-        let curr = transition.get(&(w1.to_string(), w2.to_string())).unwrap();
-        curr.push(w3.to_string());
+    // izip!() is from itertools crate
+    for (w0, w1, w2) in izip!(&words, &words[1..], &words[2..]) {
+        // add w3 to the key (w1, w2)
+        let curr = transition
+            .entry((w0.to_string(), w1.to_string()))
+            .or_insert(vec![]);
+        curr.push(w2.to_string());
     }
 
-    println!("{:?}", words);
+    // pick a random start between 0 and words.len() - 3
+    let mut rng = thread_rng();
+    let i = rng.gen_range(0, words.len() - 3);
+
+    // grab the first three words at that location
+    let mut w0 = words[i];
+    let mut w1 = words[i + 1];
+    let mut w2 = words[i + 3];
+
+    // print current word and then a space, and update our words
+
+    for _ in 0..200 {
+        print!("{} ", w2);
+        // do we need to do this before reassigning?
+        w2 = transition
+            .get(&(w0.to_string(), w1.to_string()))
+            .unwrap()
+            .choose(&mut rng)
+            .unwrap();
+        w0 = w1;
+        w1 = w2;
+    }
 }
